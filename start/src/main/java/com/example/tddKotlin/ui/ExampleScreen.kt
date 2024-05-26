37ad0f3ab392b0342ba7314f7872e942a5cff686
nosho.hiroaki <48251111+hiroaki404@users.noreply.github.com>
2024-05-26 15:01:36 +0900

    implements start module

--- start/src/main/java/com/example/tddKotlin/ui/ExampleScreen.kt
+++ start/src/main/java/com/example/tddKotlin/ui/ExampleScreen.kt
@@ -1,14 +1,34 @@
 package com.example.tddKotlin.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.tddKotlin.ExampleUiState
 import com.example.tddKotlin.ExampleViewModel
+import com.example.tddKotlin.R
+import com.example.tddKotlin.model.Bird
+import com.example.tddKotlin.model.magamo
+import com.example.tddKotlin.model.suzume
+import com.example.tddKotlin.model.tsubame
 import com.example.tddKotlin.ui.theme.TddKotlinTheme
 
 
@@ -17,23 +37,73 @@ fun ExampleScreen(
     modifier: Modifier = Modifier,
     viewModel: ExampleViewModel = hiltViewModel()
 ) {
-    ExampleScreenContent(modifier = modifier)
+    val model by viewModel.uiState.collectAsState()
+    ExampleScreenContent(modifier = modifier, model = model)
 }
 
 @Composable
-fun ExampleScreenContent(modifier: Modifier = Modifier) {
+fun ExampleScreenContent(modifier: Modifier = Modifier, model: ExampleUiState) {
     Box(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
     ) {
-        Text("Example Screen")
+        model.birds.let { birds ->
+            LazyColumn {
+                items(birds.size) { index ->
+                    Column {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            val bird = birds[index]
+                            DescriptionSection(bird = bird)
+
+                            ImageSection(bird = bird)
+                        }
+                        HorizontalDivider()
+                    }
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun DescriptionSection(modifier: Modifier = Modifier, bird: Bird) {
+    Column(modifier = modifier) {
+        Text(bird.name)
+        Text("${bird.height} cm")
+        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+            bird.season.forEach {
+                Surface(
+                    color = Color.Green.copy(alpha = 0.3f),
+                    shape = CircleShape
+                ) {
+                    Text(it.value)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageSection(bird: Bird) {
+    AsyncImage(
+        model = bird.imageUrl,
+        placeholder = painterResource(R.drawable.ic_launcher_background),
+        contentDescription = bird.name,
+        modifier = Modifier.size(100.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ExampleScreenPreview() {
+    val model = ExampleUiState(
+        selectedBird = null,
+        selectedSeason = null,
+        birds = listOf(suzume, tsubame, magamo)
+    )
     TddKotlinTheme {
-        ExampleScreen()
+        ExampleScreenContent(model = model)
     }
 }
---
+++ start/src/test/java/com/example/tddKotlin/ExampleViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.example.tddKotlin
+
+import com.example.tddKotlin.model.magamo
+import com.example.tddKotlin.model.suzume
+import com.example.tddKotlin.model.tsubame
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@Suppress("RemoveRedundantBackticks", "NonAsciiCharacters")
+class ExampleViewModelTest {
+    private val repository = ExampleRepository()
+
+    private val exampleViewModel = ExampleViewModel(repository)
+
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @Test
+    fun `鳥を取得できる`() = runTest {
+        assertEquals(
+            ExampleUiState(
+                selectedBird = null,
+                selectedSeason = null,
+                birds = listOf(
+                    suzume,
+                    tsubame,
+                    magamo
+                )
+            ),
+            exampleViewModel.uiState.value
+        )
+    }
+}

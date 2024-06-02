2e0d4d1acd5db44cad940f3df6db5e40ae9d107d
nosho.hiroaki <nosho404@gmail.com>
2024-06-03 01:39:09 +0900

    implements first step

--- firstStep/src/test/java/com/example/tddKotlin/ExampleViewModelTest.kt
+++ firstStep/src/test/java/com/example/tddKotlin/ExampleViewModelTest.kt
@@ -3,8 +3,6 @@ package com.example.tddKotlin
 import com.example.tddKotlin.model.magamo
 import com.example.tddKotlin.model.suzume
 import com.example.tddKotlin.model.tsubame
-import com.example.tddKotlin.not_good.ExampleUiState
-import com.example.tddKotlin.not_good.ExampleViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -17,23 +15,25 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 @Suppress("RemoveRedundantBackticks", "NonAsciiCharacters")
 class ExampleViewModelTest {
-    private val repository = ExampleRepository()
+    private val repository = ExampleRepositoryImpl()
 
-    private lateinit var exampleViewModel: ExampleViewModel
+    private val exampleViewModel= ExampleViewModel(repository)
 
     private val testDispatcher = UnconfinedTestDispatcher()
 
     @Before
     fun setup() {
         Dispatchers.setMain(testDispatcher)
-        exampleViewModel = ExampleViewModel(repository)
     }
 
     @Test
     fun `鳥の一覧を取得できる`() = runTest {
+        // When
+        exampleViewModel.refresh()
+
+        // Then
         assertEquals(
             ExampleUiState(
-                selectedSeason = null,
                 birds = listOf(
                     suzume,
                     tsubame,

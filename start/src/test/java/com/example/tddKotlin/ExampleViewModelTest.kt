f7aea07432c3af8bb78a23c380b7f35ca53520a4
nosho.hiroaki <nosho404@gmail.com>
2024-06-02 23:22:11 +0900

    clean start module

--- start/src/test/java/com/example/tddKotlin/ExampleViewModelTest.kt
+++ start/src/test/java/com/example/tddKotlin/ExampleViewModelTest.kt
@@ -1,46 +1,19 @@
 package com.example.tddKotlin
 
-import com.example.tddKotlin.model.magamo
-import com.example.tddKotlin.model.suzume
-import com.example.tddKotlin.model.tsubame
-import com.example.tddKotlin.not_good.ExampleUiState
-import com.example.tddKotlin.not_good.ExampleViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Suppress("RemoveRedundantBackticks", "NonAsciiCharacters")
 class ExampleViewModelTest {
-    private val repository = ExampleRepository()
-
-    private lateinit var exampleViewModel: ExampleViewModel
-
     private val testDispatcher = UnconfinedTestDispatcher()
 
     @Before
     fun setup() {
         Dispatchers.setMain(testDispatcher)
-        exampleViewModel = ExampleViewModel(repository)
     }
 
-    @Test
-    fun `鳥を取得できる`() = runTest {
-        assertEquals(
-            ExampleUiState(
-                selectedSeason = null,
-                birds = listOf(
-                    suzume,
-                    tsubame,
-                    magamo
-                )
-            ),
-            exampleViewModel.uiState.value
-        )
-    }
 }

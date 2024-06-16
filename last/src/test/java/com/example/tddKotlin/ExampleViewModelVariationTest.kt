a0d798b0e07b8abe0680d7504f96126f103327b5
nosho.hiroaki <48251111+hiroaki404@users.noreply.github.com>
2024-06-16 17:22:00 +0900

    improve test

---
+++ last/src/test/java/com/example/tddKotlin/ExampleViewModelVariationTest.kt
@@ -0,0 +1,120 @@
+package com.example.tddKotlin
+
+import app.cash.turbine.test
+import com.example.tddKotlin.model.Season
+import com.example.tddKotlin.model.magamo
+import com.example.tddKotlin.model.suzume
+import com.example.tddKotlin.model.tsubame
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+// expectMostRecentItem()を使うと、脆いテストをもろくないテストに変えられます
+// Using expectMostRecentItem() can help you transform fragile tests into robust tests.
+@OptIn(ExperimentalCoroutinesApi::class)
+@Suppress("RemoveRedundantBackticks", "NonAsciiCharacters")
+class ExampleViewModelVariationTest {
+    private val repository = FakeRepository()
+
+    private lateinit var exampleViewModel: ExampleViewModel
+
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        exampleViewModel = ExampleViewModel(repository)
+    }
+
+    @Test
+    fun `初期状態はローディング状態となる`() = runTest {
+        exampleViewModel.uiState.test {
+            assertEquals(
+                ExampleUiState(
+                    loading = true,
+                    birds = emptyList(),
+                    selectedSeason = null
+                ),
+                expectMostRecentItem()
+            )
+        }
+    }
+
+    @Test
+    fun `鳥の一覧を取得できる`() = runTest {
+        exampleViewModel.uiState.test {
+            // Given
+
+            // When
+            repository.emitValue()
+
+            // Then
+            assertEquals(
+                ExampleUiState(
+                    loading = false,
+                    birds = listOf(
+                        suzume,
+                        tsubame,
+                        magamo
+                    ),
+                    selectedSeason = null
+                ),
+                expectMostRecentItem()
+            )
+        }
+    }
+
+    @Test
+    fun `鳥を春の季節でクエリーできる`() = runTest {
+        exampleViewModel.uiState.test {
+            // Given
+            repository.emitValue()
+
+            // When
+            exampleViewModel.queryBySeason(Season.SPRING)
+
+            // Then
+            assertEquals(
+                ExampleUiState(
+                    loading = false,
+                    selectedSeason = Season.SPRING,
+                    birds = listOf(
+                        suzume,
+                        tsubame
+                    )
+                ),
+                expectMostRecentItem()
+            )
+        }
+    }
+
+    // 不安な場合はテストを追加してもよい
+    @Test
+    fun `鳥を冬の季節でクエリーできる`() = runTest {
+        exampleViewModel.uiState.test {
+            // Given
+            repository.emitValue()
+
+            // When
+            exampleViewModel.queryBySeason(Season.WINTER)
+
+            // Then
+            assertEquals(
+                ExampleUiState(
+                    loading = false,
+                    selectedSeason = Season.WINTER,
+                    birds = listOf(
+                        suzume,
+                        magamo
+                    )
+                ),
+                expectMostRecentItem()
+            )
+        }
+    }
+}
